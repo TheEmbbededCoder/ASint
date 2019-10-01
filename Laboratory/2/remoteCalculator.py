e24c06ad50b5ba2933c294dc1715c2e19058afca
francisco <francisco.mendes@eyeware.tech>
2019-10-01 13:13:55 +0100

    end lab

--- Laboratory/2/remoteCalculator.py
+++ Laboratory/2/remoteCalculator.py
@@ -1,10 +1,17 @@
 import socket
 
+
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = socket.gethostname()
-port = 12345
+port = 12343
 s.connect((host, port))
-msg = input()
-byt = msg.encode()
-s.send(byt)
+while 1:
+	msg = input()
+	byt = msg.encode()
+	s.send(byt)
+	if msg == 'exit':
+		break
+	elif msg == 'pop':
+		result = s.recv(1024)
+		print("Result: ", result.decode())
 s.close 
--- Laboratory/2/rpnCalculator.py
+++ Laboratory/2/rpnCalculator.py
@@ -15,14 +15,20 @@ class rpnCalculator:
 			print("Empty Stack")
 
 	def add(self):
-		a = self.memory.pop()
-		b = self.memory.pop()
-		self.memory.append(a+b)
+		try:
+			a = self.memory.pop()
+			b = self.memory.pop()
+			self.memory.append(a+b)
+		except:
+			print("Empty Stack")
 
 	def sub(self):
-		a = self.memory.pop()
-		b = self.memory.pop()
-		self.memory.append(a-b)
+		try:
+			a = self.memory.pop()
+			b = self.memory.pop()
+			self.memory.append(a-b)
+		except:
+			print("Empty Stack")
 
 	def printStack(self):
 		print(self.memory)
@@ -33,45 +39,50 @@ if __name__ == '__main__':
 
 	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	host = socket.gethostname()
-	port = 12345
+	port = 12343
 	s.bind((host, port))
 	s.listen(5)
 	while True:
 		c, addr = s.accept()
 		print('Connection from', addr)
 
-		msg = c.recv(1024)
-		print("Received: ", msg.decode())
-		msg = msg.decode()
-		msg = msg.split()
+		while True:
+			msg = c.recv(1024)
+			print("Received: ", msg.decode())
+			msg = msg.decode()
+			msg = msg.split()
 
-		if msg[0] == 'push':
-			calculator.pushValue(float(msg[1]))
-		elif msg[0] == 'pop':
-			calculator.popValue()
-		elif msg[0] == 'add':
-			calculator.add()
-		elif msg[0] == 'sub':
-			calculator.add()
-		else:
-			calculator.printStack()
+			if msg[0] == 'push':
+				calculator.pushValue(float(msg[1]))
+			elif msg[0] == 'pop':
+				result = str(calculator.popValue())
+				byt = result.encode()
+				c.send(byt)
+			elif msg[0] == 'add':
+				calculator.add()
+			elif msg[0] == 'sub':
+				calculator.add()
+			elif msg[0] == 'exit':
+				break
+			else:
+				calculator.printStack()
 
 		c.close()
 
 
-	calculator.pushValue(1)
-	calculator.pushValue(2)
-	calculator.pushValue(3)
-	calculator.pushValue(4)
-	calculator.pushValue(5)
-	print("Stack: ", calculator.printStack())
-	print("Pop value: ", calculator.popValue())
-	print("Stack: ", calculator.printStack())
-	print("Add: ", calculator.add())
-	print("Stack: ", calculator.printStack())
-	print("Pop value: ", calculator.popValue())
-	print("Stack: ", calculator.printStack())
-	print("Sub: ", calculator.sub())
-	print("Stack: ", calculator.printStack())
-	print("Pop value: ", calculator.popValue())
-	print("Stack: ", calculator.printStack())
+	#	calculator.pushValue(1)
+	#	calculator.pushValue(2)
+	#	calculator.pushValue(3)
+	#	calculator.pushValue(4)
+	#	calculator.pushValue(5)
+	#	print("Stack: ", calculator.printStack())
+	#	print("Pop value: ", calculator.popValue())
+	#	print("Stack: ", calculator.printStack())
+	#	print("Add: ", calculator.add())
+	#	print("Stack: ", calculator.printStack())
+	#	print("Pop value: ", calculator.popValue())
+	#	print("Stack: ", calculator.printStack())
+	#	print("Sub: ", calculator.sub())
+	#	print("Stack: ", calculator.printStack())
+	#	print("Pop value: ", calculator.popValue())
+	#	print("Stack: ", calculator.printStack())
