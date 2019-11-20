3cf46b7e3e3e907ceefe34f60e739e6f48dae4c2
gccunha1 <gonca@LAPTOP-6ARAGME1.localdomain>
2019-11-20 00:42:45 +0000

    faz o display da ementa em função da data

--- Project/Microservices/Canteen/canteenMicroservices.py
+++ Project/Microservices/Canteen/canteenMicroservices.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request, url_for, redirect
 from flask import jsonify
+import json
 
 import requests
 
@@ -9,10 +10,13 @@ app = Flask(__name__)
 
 ########## REST API ###########
 
-@app.route('/cateen')
+@app.route('/canteen')
 def API_showAll():
 	# Show all canteens
-	canteen = None
+	print('API_showCanteen')
+	resp = requests.get('https://fenix.tecnico.ulisboa.pt/api/fenix/v1/canteen')
+	canteen = resp.json()
+	print(canteen)
 	if canteen == None:
 		message = {
 		'status_code': 404,
@@ -27,47 +31,38 @@ def API_showAll():
 		}
 	return jsonify(message)
 
-@app.route('/cateen/<location>')
-def API_showCateen(location):
-	print('API_showCateen ' + str(location))
-	resp = requests.get('https://fenix.tecnico.ulisboa.pt/api/fenix/' + str(location) + '/canteen')
+@app.route('/canteen/<date>')
+def API_showCanteen(date):
+	print(date)
+	date = date[0:2] + '/' + date[2:4] + '/' + date[4:8]
+	
+	resp = requests.get('https://fenix.tecnico.ulisboa.pt/api/fenix/v1/canteen')
 	if resp.status_code != 200:
 	    # This means something went wrong.
 	    pass
 	else:
 	    print(resp)
-	canteen = None
-	if canteen == None:
+	
+	canteen = resp.json()
+	output = None
+	for d in canteen:
+		if d['day'] == date:
+			output = d['meal']
+	if output == None:
 		message = {
 		'status_code': 404,
 		'message': 'No resource found',
-		'canteen': None
+		'meal': None
 		}
 	else:
 		message = {
 		'status_code': 200,
 		'message': 'OK',
-		'canteen': canteen
+		'meal': output
 		}
-	return jsonify(message)
+	return message
 	
-@app.route('/cateen/<location>/<day>/<mounth>/<year>')
-def API_showMeal(location, day, mounth, year):
-	# Show all canteens
-	canteen = None
-	if canteen == None:
-		message = {
-		'status': 404,
-		'message': 'No resource found',
-		'canteen': None
-		}
-	else:
-		message = {
-		'status': 200,
-		'message': 'OK',
-		'canteen': canteen
-		}
-	return jsonify(message)
+
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
